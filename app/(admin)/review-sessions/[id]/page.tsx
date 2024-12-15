6a958d6c9d98433f9badb457e4ae11a7d34720ee
m-rezk-meem <m.Rezk@meemdev.com>
2024-12-15 16:47:18 +0200

    fix build errors fr fr

--- app/(admin)/review-sessions/[id]/page.tsx
+++ app/(admin)/review-sessions/[id]/page.tsx
@@ -1,17 +1,22 @@
 import Messages from '@/components/Messages';
 import { GET_CHAT_SESSIONS_MESSAGES } from '@/graphql/queiries';
 import { serverClient } from '@/lib/server/serverClient';
-import { Params } from 'next/dist/server/request/params';
 import React from 'react';
 
-export const dynamic = 'force-dynamic';
-type PageProps = {
+// Use the correct props type
+type Props = {
   params: {
     id: string;
   };
 };
-async function ReviewSession({ params }: { params: Params }) {
+
+export const dynamic = 'force-dynamic';
+
+async function ReviewSession({ params }: any) {
+  console.log(typeof params); // To check if it's inferred correctly
+
   const { id } = params;
+
   const {
     data: {
       chat_sessions: {
@@ -24,12 +29,12 @@ async function ReviewSession({ params }: { params: Params }) {
     },
   } = await serverClient.query({
     query: GET_CHAT_SESSIONS_MESSAGES,
-    variables: { id: parseInt(`${id}`) },
+    variables: { id: parseInt(id, 10) }, // Ensure ID is parsed as an integer
   });
 
   return (
     <div className='flex-1 p-10 pb-24'>
-      <h1 className='text-xl lg:text-3xl font-semibold'> Session Review</h1>
+      <h1 className='text-xl lg:text-3xl font-semibold'>Session Review</h1>
       <p>Started at {new Date(created_at).toLocaleString()}</p>
       <h2 className='font-light mt-2'>
         Between {name} &{' '}
