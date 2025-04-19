1aeb37fe426a0bfcdfdcfc0dc77e9bbd3bf68ce7
Mohamedrezk-web <mohamedrizkmr6@gmail.com>
2025-04-19 10:01:39 +0200

    revert changes

--- graphql/apolloClient.ts
+++ graphql/apolloClient.ts
@@ -7,8 +7,7 @@ import {
 
 export const BASE_URL =
   process.env.NODE_ENV !== 'development'
-    ? process.env.NEXT_PUBLIC_APP_URL ||
-      `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
     : 'http://localhost:3000';
 
 const httpLink = createHttpLink({
